73a5a92ae773b5bd3bc5ada433c26fdbea760fcd
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-06-13 09:58:59 +0530

    Adds Product model

---
+++ ProductService/Models/Model.cs
@@ -0,0 +1,10 @@
+namespace ProductService.Models;
+
+public class Model
+{
+    int Id { get; set; }
+    string Name { get; set; }
+    double Price { get; set; }
+    int Quantity { get; set; }
+    string Category { get; set; }
+}
